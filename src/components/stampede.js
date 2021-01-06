1c372baa0f1bcb9c1acf8113dc6a73d93cbd5c1a
Jason Lengstorf <jason@lengstorf.com>
2021-01-05 18:01:46 -0800

    fix: stop extra stampede label from showing up

--- src/components/stampede.js
+++ src/components/stampede.js
@@ -1,18 +1,30 @@
 import { h } from 'preact';
-import { useEffect } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 
 export function Stampede() {
+  const [initialized, setInitialized] = useState(false);
+  const [text, setText] = useState();
+
   useEffect(() => {
+    if (initialized) {
+      return;
+    }
+
     import('/web_modules/splitting.js').then((importedModule) => {
       const splitting = importedModule.default;
 
-      splitting({ by: 'chars' });
+      // doing it this way avoids an issue where Splitting and React get
+      // out of sync and end up duplicating text
+      const htmlToSplit = `
+        <p data-splitting>STAMPEDE!</p>
+      `;
+
+      setText(splitting.html({ content: htmlToSplit, by: 'chars' }));
+      setInitialized(true);
     });
   }, []);
 
   return (
-    <div className="stampede">
-      <p data-splitting>STAMPEDE!</p>
-    </div>
+    <div className="stampede" dangerouslySetInnerHTML={{ __html: text }} />
   );
 }
